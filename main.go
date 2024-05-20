5435746fa471c8934e3a690bf6588bdf7aa8b196
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2024-05-20 21:53:04 +0100

    just getting the hang of the basics

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 import "fmt"
+import "strings"
 
 func main(){
 	conferenceName := "Go Conference"
-	const totalTickets int = 50
-	var remainingTickets int = 50
+	const totalTickets uint = 50
+	var remainingTickets uint = 50
 	
 	println("Welcome to",conferenceName,"the platform")
 	println("Get you tickets here!")
@@ -12,21 +13,35 @@ func main(){
 	fmt.Printf("Total number of tickets %v available tickets %v \n", 
 	totalTickets, remainingTickets)
 
+	users := []string{}
 	var userName string
-	var userTickets int
+	var userTickets uint
+	fmt.Println("Welcome Buy your Tickets here!!!!!")
+	top:
+	for{
+		fmt.Print("Enter your name:")
+		fmt.Scan(&userName)
+		fmt.Print("Enter no of Tickets:")
+		fmt.Scan(&userTickets)
 
-	fmt.Print("Enter your name:")
-	fmt.Scan(&userName)
-	fmt.Print("Enter no of Tickets:")
-	fmt.Scan(&userTickets)
+		if (userTickets <= remainingTickets){
+			remainingTickets -= userTickets
+			fmt.Println("Successfull purchase!!")
+			fmt.Printf("\nUser %v booked %v tickets\n", userName, userTickets)
+			users = append(users, userName)
+		} else
+		{
+			fmt.Printf("\nUser %v cannot book more tickets than available tickets: %v\n", 
+			userName, remainingTickets)
+		}
+		var flag string
+		fmt.Print("Do you want to continue:(y/n)")
+		fmt.Scan(&flag)
 
-	if (userTickets <= remainingTickets){
-		remainingTickets -= userTickets
-		fmt.Println("Successfull purchase!!")
-		fmt.Printf("\nUser %v booked %v tickets\n", userName, userTickets)
-	} else
-	{
-		fmt.Printf("\nUser %v cannot book more tickets than available tickets: %v\n", 
-		userName, remainingTickets)
+		if (flag == string('n')){
+			fmt.Println("Booked list:")
+			fmt.Println(strings.Join(users[:], ","))
+			break top
+		}
 	}
 }
